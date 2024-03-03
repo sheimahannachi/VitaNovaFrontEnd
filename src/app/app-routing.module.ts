460a312c1b1d6c8b3696d925d1e809c25a0a422f
SnapeV1 <Hamada_amine@yahoo.com>
2024-03-03 18:20:42 +0100

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllTemplateFrontComponent } from './front-office/all-template-front/all-template-front.component';
+import { RegisterComponent } from './front-office/user/register/register.component';
+import { LoginComponent } from './front-office/user/login/login.component';
+import { AllTemplateBackComponent } from './back-office/all-template-back/all-template-back.component';
+import { UsersBackComponent } from './back-office/users-back/users-back.component';
+const routes: Routes = [
+  { path: '', component: AllTemplateFrontComponent },
+  { path: 'signup', component: RegisterComponent } ,
+  { path: 'login', component: LoginComponent } ,
+{path:'admin',component:AllTemplateBackComponent},
+{path:'admin/users',component:UsersBackComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './front-office/navbar/navbar.component';
+import { FooterComponent } from './front-office/footer/footer.component';
+import { AllTemplateFrontComponent } from './front-office/all-template-front/all-template-front.component';
+
+import { UserComponent } from './front-office/user/user.component';
+import { RegisterComponent } from './front-office/user/register/register.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './front-office/user/login/login.component';
+import { NavBarBackComponent } from './back-office/nav-bar-back/nav-bar-back.component';
+import { AllTemplateBackComponent } from './back-office/all-template-back/all-template-back.component';
+import { FooterBackComponent } from './back-office/footer-back/footer-back.component';
+import { UsersBackComponent } from './back-office/users-back/users-back.component';
+import { SideBarBackComponent } from './back-office/side-bar-back/side-bar-back.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AllTemplateFrontComponent,
+    
+   
+    UserComponent,
+    RegisterComponent,
+    LoginComponent,
+    NavBarBackComponent,
+    AllTemplateBackComponent,
+    FooterBackComponent,
+    UsersBackComponent,
+    SideBarBackComponent,
+ 
+    
+    
+    
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
