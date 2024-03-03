02cca7ef46a20bd0deaa2c4237f7bb0f84d35302
yoserzayani <yosr.zayani@esprit.tn>
2024-03-04 00:56:31 +0100

    backOffice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AllTemplateBackComponent} from "./back-office/all-template-back/all-template-back.component";
+//import {AllTemplateFrontComponent} from "./FrontOffice/all-template-front/all-template-front.component";
+import {AddexerciseComponent} from "./addExercise/addexercise.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*{
+    path:"",
+    component:AllTemplateFrontComponent
+  },*/
+  {
+    path:"admin",
+    component:AllTemplateBackComponent, children: [
+      {
+        path: "exercise",
+        component: AddexerciseComponent,
+      }]
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
