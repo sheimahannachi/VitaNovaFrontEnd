e20ed6f5e7aa80d07bc53556be746291fc8732cf
SnapeV1 <Hamada_amine@yahoo.com>
2024-03-11 15:39:29 +0100

    login/signup/reset/Code

---
+++ src/app/Service/user.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import{UserModule} from '../Models/user.module'
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private baseAdminUrl: string = 'http://localhost:8081/api/user/admin';
+  private baseUrl: string ='http://localhost:8081/api';
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<UserModule[]> {
+    return this.http.get<UserModule[]>(`${this.baseAdminUrl}/AllUsers`);
+
+  }
+  deleteUser(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseAdminUrl}/DeleteUser/${id}`);
+  }
+  ActivateUser(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseAdminUrl}/ActivateUser/${id}`);
+  }
+
+  updateUser(user: UserModule): Observable<UserModule> {
+    return this.http.put<UserModule>(`${this.baseAdminUrl}/UpdateUser`, user);
+  }
+  resetPassword(email:string,password:string): Observable<void>{
+    let payload = {
+      email,
+      password
+    }
+        return this.http.put<void>(`${this.baseUrl}/`,payload);
+    
+  }
+}
--- src/app/front-office/user/register/register.component.ts
+++ src/app/front-office/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-register',
@@ -17,13 +18,18 @@ export class RegisterComponent {
   picture: string ="";
   gender:string="";
   weight:number=0;
+  
   height:number=0;
-
-
-  constructor(private http: HttpClient )
-  {
+  selectedFile: File | null = null;
+  showVerificationCodeInput: boolean = false;
+  verificationCode!: string;
+  generatedCode!: string;
+  show_verification:boolean=false;
+verified:boolean=false;
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {
 
   }
+
   save()
   {
   
@@ -38,14 +44,93 @@ export class RegisterComponent {
       "weight":this.weight,
       "dateOfBirth":this.dateOfBirth,
       "firstName":this.firstName,
-      "lastName":this.lastName
+      "lastName":this.lastName,
+      "verified":this.verified
     };
+    
+    if(this.verified==true){
     this.http.post("http://localhost:8081/api/signup",bodyData,{responseType: 'text'}).subscribe((resultData: any)=>
     {
-        console.log(resultData);
+
+        console.log(bodyData);
         alert(" Registered Successfully");
 
     });
+  }}
+
+ verifyEmail() {
+    if (this.verificationCode === this.generatedCode) {
+      // If the codes match, set verified to true
+      this.verified = true;
+      console.log('Email verified successfully');
+    } else {
+      console.log('Verification code does not match');
+    }
+    console.log(this.verificationCode);
+    console.log(this.generateVerificationCode);
+
   }
 
+
+
+
+  sendVerificationCode() {
+    this.generatedCode=this.generateVerificationCode();
+this.showVerificationCodeInput=true;
+    // Make HTTP POST request to your backend endpoint
+    const apiUrl = 'http://localhost:8081/api/sendEmail';
+    const payload = { to: this.email, subject:"verification",text: this.generatedCode };
+
+    this.http.post(apiUrl, payload).subscribe(
+      (response) => {
+        console.log('Verification code sent successfully');
+        // Handle success (e.g., show a success message to the user)
+      },
+      (error) => {
+        console.error('Error sending verification code:', error);
+        // Handle error (e.g., show an error message to the user)
+      }
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  onFileChanged(event: any): void {
+    this.selectedFile = event.target.files[0];
+  }
+
+
+getSelectedFileUrl(): SafeUrl | null {
+    if (this.selectedFile) {
+      const url = window.URL.createObjectURL(this.selectedFile);
+      return this.sanitizer.bypassSecurityTrustUrl(url);
+    } else {
+      return null;
+    }
+  }
+
+
+  generateVerificationCode(): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let verificationCode = '';
+    for (let i = 0; i < 6; i++) {
+      verificationCode += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return verificationCode;
+  }
+  
+
+
+  
 }
